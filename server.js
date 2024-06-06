af8a6c5aca6fa9c6074030553a0be07cba7c9fba
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2024-06-06 14:58:46 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('offer', (offer) => {
+        socket.broadcast.emit('offer', offer);
+    });
+
+    socket.on('answer', (answer) => {
+        socket.broadcast.emit('answer', answer);
+    });
+
+    socket.on('candidate', (candidate) => {
+        socket.broadcast.emit('candidate', candidate);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
